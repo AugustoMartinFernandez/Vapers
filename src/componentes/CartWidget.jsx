a2b692e418ac976e0b519c0d99959dfb07d2a46d
Augusto Martin Fernandez <martinnfernandezz.20@gmail.com>
2024-10-05 12:56:59 -0300

    Segundo commit jajaj

--- src/componentes/CartWidget.jsx
+++ src/componentes/CartWidget.jsx
@@ -3,7 +3,7 @@ import { FaShoppingCart } from 'react-icons/fa';
 
 const CartWidget = () => {
   return (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
+    <div style={{ display: 'flex', alignItems: 'center', color:"black" }}>
       <FaShoppingCart size={24} />
       <span style={{ marginLeft: '5px' }}>0</span> 
     </div>
